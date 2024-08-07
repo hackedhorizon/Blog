dd71c70c2ddbdcda3c6d76a43e54f3cc9ea0ca00
cybernexus <raczdwork@proton.me>
2024-08-07 20:22:24 +0200

    Add loading indicator for the profile component update button.

--- resources/views/components/buttons/primary-button.blade.php
+++ resources/views/components/buttons/primary-button.blade.php
@@ -2,6 +2,7 @@
     @if (isset($target)) wire:target='{{ $target }}' @endif
     @if (isset($click)) wire:click="{{ $click }}" @endif>
 
-    <span wire:loading.remove wire:target="login, register">{{ $translation }}</span>
-    <span wire:loading wire:target="login, register" class="loading loading-spinner loading-md"></span>
+    <span wire:loading.remove wire:target="login, register, updateProfileInformation">{{ $translation }}</span>
+    <span wire:loading wire:target="login, register, updateProfileInformation"
+        class="loading loading-spinner loading-md"></span>
 </button>
