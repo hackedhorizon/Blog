dd55cfaee217169854e92def22c4f738bb274865
cybernexus <raczdwork@proton.me>
2024-08-26 18:23:49 +0200

    - Minor change in UI

--- resources/views/components/cards/article-card-container.blade.php
+++ resources/views/components/cards/article-card-container.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col max-w-screen-xl gap-10 p-5 m-5 tracking-normal shadow-lg bg-surface-200/50">
+<div class="flex flex-col max-w-screen-xl gap-10 p-5 m-5 tracking-normal shadow-lg bg-surface-200">
     {{-- Header with title and explore more link --}}
     <div class="flex flex-row items-end justify-between">
         <h1 class="text-2xl md:text-4xl">{{ $title }}</h1>
--- resources/views/livewire/features/language-switcher.blade.php
+++ resources/views/livewire/features/language-switcher.blade.php
@@ -1,6 +1,7 @@
 <div class="flex flex-col w-screen text-center">
     <label for="language">{{ __('Language') }}</label>
-    <select id='language' wire:model.live='selectedLanguage' class="w-full max-w-xs mx-auto select select-info">
+    <select id='language' wire:model.live='selectedLanguage'
+        class="w-full max-w-xs mx-auto select select-info bg-surface-300">
         @foreach ($languages as $key => $value)
             <option value="{{ $key }}">{{ $value }}</option>
         @endforeach
